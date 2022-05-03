1ec741291c7340efce55fe419063d6a83ec57b90
Olaf Lischke <olaf.lischke@lischke-edv.de>
2022-05-03 10:30:54 +0200

    Berge aus der Mondial

--- Live Coding/MondialKonsole/MondialKonsole/Program.cs
+++ Live Coding/MondialKonsole/MondialKonsole/Program.cs
@@ -22,14 +22,33 @@ namespace MondialKonsole
                                                         .Where(el => el.Name == "name")
                                                         .Select(el => el.Value).FirstOrDefault(),
                                             Population = Convert.ToInt32(xe.Elements()
-                                                            .Where(el => el.Name == "population")
-                                                            .Select(el => el.Value).LastOrDefault())
+                                                                            .Where(el => el.Name == "population")
+                                                                            .Select(el => el.Value).LastOrDefault())
                                         }
-                                         );
+                                         ).OrderByDescending(land => land.Population);
 
             foreach (var item in qLaender)
             {
                 Console.WriteLine($"{item.Name}: {item.Population:#,##0}");
+            }
+
+            var qBerge = document.Root.Descendants()
+                                    .Where(xe => xe.Name == "mountain")
+                                    .Select(xe => new
+                                    {
+                                        Name = xe.Elements()
+                                                        .Where(el => el.Name == "name")
+                                                        .Select(el => el.Value).FirstOrDefault(),
+                                        Elevation = Convert.ToInt32(xe.Elements()
+                                                                        .Where(el => el.Name == "elevation")
+                                                                        .Select(el => el.Value).FirstOrDefault())
+                                    })
+                                    .OrderByDescending(berg => berg.Elevation)
+                                    .Take(10);
+
+            foreach (var item in qBerge)
+            {
+                Console.WriteLine($"{item.Name}: {item.Elevation:#,##0}");
             }
 
             Console.ReadKey();
